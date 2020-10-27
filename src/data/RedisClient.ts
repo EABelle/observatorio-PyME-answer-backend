98a01b31d12ab7b67b013a3f505abb0588854b37
Esteban Bellegarde <esteban.bellegarde@gmail.com>
2020-10-26 21:21:54 -0300

    src data folder missignored by git

---
+++ src/data/RedisClient.ts
@@ -0,0 +1,19 @@
+const redis = require(process.env.NODE_ENV === 'test' ? 'redis-mock' : 'redis');
+const { promisify } = require('util');
+
+const options = {
+    host: !process.env.NODE_ENV || process.env.NODE_ENV  === 'development' ? 'localhost' : process.env.REDIS_BASE_URL,
+    port: process.env.REDIS_PORT || 6379,
+};
+
+const client = redis.createClient(options);
+client.on('connect', () => {
+    console.log('Connected to cache - host: ' + options.host + ' port: ' + options.port );
+});
+
+export const getAsync = promisify(client.get).bind(client);
+export const setAsync = promisify(client.set).bind(client);
+export const keysAsync = promisify(client.keys).bind(client);
+export const expireAsync = promisify(client.expire).bind(client);
+
+export default client;
---
+++ src/data/repository/user.repository.ts
@@ -0,0 +1,16 @@
+import {UserModel} from '../../core/model/user.model';
+import {User} from '../../core/domain/User';
+import {UserPayload} from '../../api/contract';
+
+export class UserRepository {
+    static async create(userPayload: UserPayload): Promise<User> {
+        const userModel = new UserModel(userPayload);
+        const user = await userModel.save();
+        return user.toObject();
+    }
+
+    static async getByEmail(email: String): Promise<User> {
+        const user = await UserModel.findOne({email});
+        return user.toObject();
+    }
+}
