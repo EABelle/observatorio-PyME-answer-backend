297605b071a7efbc918a286024f0ef2487866308
Esteban Bellegarde <esteban.bellegarde@gmail.com>
2020-10-26 21:42:17 -0300

    manage redis cloud url

--- src/data/RedisClient.ts
+++ src/data/RedisClient.ts
@@ -1,14 +1,16 @@
 const redis = require(process.env.NODE_ENV === 'test' ? 'redis-mock' : 'redis');
 const { promisify } = require('util');
 
-const options = {
-    host: !process.env.NODE_ENV || process.env.NODE_ENV  === 'development' ? 'localhost' : process.env.REDIS_BASE_URL,
-    port: process.env.REDIS_PORT || 6379,
-};
+const options = process.env.REDISCLOUD_URL ?
+    { url: process.env.REDISCLOUD_URL }
+    : {
+        host: !process.env.NODE_ENV || process.env.NODE_ENV  === 'development' ? 'localhost' : process.env.REDIS_BASE_URL,
+        port: process.env.REDIS_PORT || 6379,
+    };
 
 const client = redis.createClient(options);
 client.on('connect', () => {
-    console.log('Connected to cache - host: ' + options.host + ' port: ' + options.port );
+    console.log('Connected to Redis cache');
 });
 
 export const getAsync = promisify(client.get).bind(client);
